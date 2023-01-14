bdc22d4dab8eab07881ae658df0a8cf4fb796c43
yanghojoon <yanghojoon8487@gmail.com>
2023-01-15 00:17:42 +0900

    Initial Commit

---
+++ StanfordSwiftUI/StanfordSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordSwiftUIApp.swift
+//  StanfordSwiftUI
+//
+//  Created by 양호준 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct StanfordSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
